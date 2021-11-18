15dba36bddece9e8a142c131ab5753c89cb8a07e
Ivan Jatz <jatzivan@gmail.com>
2021-11-18 01:42:26 +0100

    added basic states for goblin

---
+++ Assets/Scripts/MonsterScripts/MeleeMonsterAtackScript.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeMonsterAtackScript : MonoBehaviour
+{
+
+
+    public float baseDmg = 10f;
+    public float currentDmg = 10f;
+
+    public float currentAttackSpeed = 1f;
+    public float baseAttackSpeed = 1f;
+
+    public float lightAttackDuration = 0.5f;
+
+    private float lightAttackCoolDown = 1;
+    
+    private float lightAttackC = 0f;
+    private float lightAttackD = 0f;
+
+    public AttackState currectAttackState = AttackState.READY;
+    private MonsterAI monsterBrain;
+    private Animator monsterAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        monsterBrain = gameObject.GetComponent<MonsterAI>();
+        monsterAnimator = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        checkAttack();
+    }
+
+    private bool canAttack()
+    {
+        return true;
+    }
+
+    private void checkAttack()
+    {
+        switch (currectAttackState)
+        {
+            case AttackState.READY:
+
+                if (monsterBrain.currentState.Equals(MonsterAI.MY_STATE.ATTACKING) && canAttack())
+                {
+                    monsterAnimator.SetTrigger("attack");
+                    currectAttackState = AttackState.ATTACKING;
+                    lightAttackD = lightAttackDuration;
+
+
+                }
+
+                break;
+            case AttackState.ON_COOLDOWN:
+                lightAttackC -= Time.deltaTime;
+                if (lightAttackC <= 0)
+                {
+                    currectAttackState = AttackState.READY;
+                }
+
+                break;
+            case AttackState.ATTACKING:
+                lightAttackD -= Time.deltaTime;
+
+                if (lightAttackD <= 0)
+                {
+                    currectAttackState = AttackState.ON_COOLDOWN;
+                    lightAttackC = (lightAttackCoolDown - currentAttackSpeed);
+                }
+                break;
+        }
+    }
+
+    void Attack()
+    {
+        Collider2D[] enemyToHit = Physics2D.OverlapCircleAll(monsterBrain.damageCircle.position, monsterBrain.damageRange, monsterBrain.enemyLayers);
+        if (enemyToHit != null && enemyToHit.Length > 0)
+        {
+            enemyToHit[0].gameObject.GetComponent<PlayerHealth>().takeDamage(10);
+        }
+    }
+
+    public enum AttackState
+    {
+        READY,
+        ATTACKING,
+        ON_COOLDOWN
+    }
+
+}
--- Assets/Scripts/MonsterScripts/MonsterAI.cs
+++ Assets/Scripts/MonsterScripts/MonsterAI.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 public class MonsterAI : MonoBehaviour
 {
 
+    public Transform damageCircle;
+    public float damageRange = 0.5f;
+    public LayerMask enemyLayers;
+
+    public Transform enemyDetectionCircle;
+    public float detectionRange = 0.5f;
+
+    public MY_STATE currentState = MY_STATE.IDLE;
 
 
     // Start is called before the first frame update
@@ -16,14 +24,59 @@ public class MonsterAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        detectAttack();
+        if (currentState.Equals(MY_STATE.IDLE)){
+
+             detectEnemies();
+        }
     }
 
-    public enum ENEMY_STATE
+    private void detectEnemies()
+    {
+        Collider2D[] enemyInRange = Physics2D.OverlapCircleAll(enemyDetectionCircle.position, detectionRange, enemyLayers);
+        if (enemyInRange != null && enemyInRange.Length > 0)
+        {
+            currentState = MY_STATE.CLOSING_DISTANCE;
+        }
+        else
+        {
+            currentState = MY_STATE.IDLE;
+        }
+    }
+
+    private void detectAttack()
+    {
+        Collider2D[] enemyInRange = Physics2D.OverlapCircleAll(damageCircle.position, damageRange, enemyLayers);
+        if (enemyInRange != null && enemyInRange.Length > 0)
+        {
+            currentState = MY_STATE.ATTACKING;
+        }
+        else
+        {
+            currentState = MY_STATE.IDLE;
+        }
+    }
+
+    public enum MY_STATE
     {
         IDLE,
         RUNNING_AWAY,
+        CLOSING_DISTANCE,
         ATTACKING
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if (damageCircle != null)
+        {
+            Gizmos.DrawWireSphere(damageCircle.position, damageRange);
+        }
+
+        if(enemyDetectionCircle != null)
+        {
+            Gizmos.DrawWireSphere(enemyDetectionCircle.position, detectionRange);
+        }
+
     }
 
 }
